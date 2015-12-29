2b97563b80502dd9044ee9e489fb6fa55e199f2e
Nate Marks <nate.marks@pobox.com>
2015-12-29 07:30:26 -0500

    Update watch_tcp.sh
    dealing with time, redirect and exit codes is such a PITA.  This too way too long and using the function was a way of giving up, really.  I'm keeping this one around because of the pain

--- watch_tcp.sh
+++ watch_tcp.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+function connect {
+    nc -w 3 -zv $1 $2 > /dev/null
+    if [ $? -gt 0 ]
+    then
+        echo "FAILED AT: ${TIME}" >> watch_tcp.log
+    else
+        echo "SUCCEEDED AT: ${TIME}" >> watch_tcp.log
+    fi
+}
 TIME=$(date)
 echo "ATTEMPT: ${TIME}" >> watch_tcp.log
-DURATION=`(time nc -w 3 -zv $1 $2  > /dev/null;EXIT=$?) 2>&1 | grep real`
-if [ ${EXIT} !=  0 ]
-then
-    echo "FAILED AT: ${TIME}" >> watch_tcp.log
-else
-    echo "SUCCEEDED AT: ${TIME}" >> watch_tcp.log
-fi
+DURATION=`(time (connect $1 $2 )) 2>&1 | grep real`
 echo ${DURATION} >> watch_tcp.log
