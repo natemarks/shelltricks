bbce7841febe9a6deca4c2285419d63601d83ab1
Nate Marks <nmarks@granularnetworks.com>
2016-10-08 06:58:56 -0400

    refactoring

--- move-rrds.py
+++ move-rrds.py
@@ -30,9 +30,23 @@ class RRDTree(object):
                       '-name',
                       'NWN-DEVICE-PING-STATS_rtt_min.rrd'],
                      stdout=PIPE)
-        self.find_output = proc.communicate()[0].split()
+        self.source_files = proc.communicate()[0].split()
 
     def make_destination_tree(self):
+        self.dest_dirs = []
+        if self.source_files == []:
+            self.source_files = ['/opt/zenoss/aaa/bbb/ccc/filename.txt']
+        for line in self.source_files:
+            targ_file = self.src_file_to_dest_file(line)
+            print "======================"
+            print "MOVING: " + line
+            print "TO: " + targ_file
+            print "======================"
+            targ_dir = self.strip_file_from_path(targ_file)
+            if targ_dir not in self.dest_dirs:
+                print "CREATING: " + targ_dir
+                self.dest_dirs.append(targ_dir)
+
         print self.prepend_dest_prefix()
 
     def strip_source_prefix(self):
@@ -43,9 +57,9 @@ class RRDTree(object):
         beginning of each entry
         '''
         dest_list = []
-        if self.find_output == []:
-            self.find_output = ['/opt/zenoss/aaa/bbb/ccc/']
-        for line in self.find_output:
+        if self.source_files == []:
+            self.source_files = ['/opt/zenoss/aaa/bbb/ccc/filename.txt']
+        for line in self.source_files:
             dest_list.append(line[len(self.source):])
         # remove duplicates by converting to set and back to list
         dest_list = list(set(dest_list))
@@ -64,7 +78,40 @@ class RRDTree(object):
             prepended_list.append("/" + "/".join(parts))
         return prepended_list
 
+    def src_file_to_dest_file(self, filepath):
+        '''
+        strip  a number of chars form the beginning equal to the length of the
+        src directory
+
+        tack on a leading slash, then the dst directory, then another slash
+        just in case, the the stripped path
+
+        this may mean we have extra slashes like "/file//to/a/path" but the
+        filter and join lines clean those up
+
+        '''
+        stripped = filepath[len(self.source):]
+        prepended = "/" + self.destination + "/" + stripped
+        parts = prepended.split('/')
+        parts = filter(None, parts)
+        return "/" + "/".join(parts)
+
+    def strip_file_from_path(self, filepath):
+        parts = filepath.split('/')
+        parts.pop()
+        return "/" + "/".join(parts)
+
+    def create_directory(self, dir):
+        proc = Popen(['mkdir',
+                      '-p',
+                      dir],
+                     stdout=PIPE)
+        output, errors = proc.communicate()
+        if proc.returncode or errors:
+            print [proc.returncode, errors, output]
+            exit
+
 
 if __name__ == "__main__":
     gg = RRDTree()
-    gg.make_destination_tree()
+    gg.create_directory("/tmpg/aaa/bbb/ccc")
